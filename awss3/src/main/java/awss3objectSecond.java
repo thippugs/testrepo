d4dace262455b2f1a16a7529958319866f97f9bd
Thippu <tippu_gs@yahoo.com>
2020-12-31 06:02:27 -0500

    new class added to FeatureTestRepo awss3objectSecond
    Signed-off-by: Thippu <tippu_gs@yahoo.com>

---
+++ awss3/src/main/java/awss3objectSecond.java
@@ -0,0 +1,13 @@
+public class awss3objectSecond {
+
+    public static void main(String args[]){
+
+        System.out.println("hello guru");
+        System.out.println("hello gurus2");
+        System.out.println("hello gurus3");
+        System.out.println("hello gurus4");
+        System.out.println("hello gurus5");
+        System.out.println("hello gurus5 FeatureBranch 1");
+
+    }
+}
