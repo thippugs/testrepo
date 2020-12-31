ee1d6e7d05fb82d9dc66617e44a56024c2514e10
Thippu <tippu_gs@yahoo.com>
2020-12-31 06:55:48 -0500

    third main method
    Signed-off-by: Thippu <tippu_gs@yahoo.com>

--- awss3/src/main/java/awss3object2.java
+++ awss3/src/main/java/awss3object2.java
@@ -19,6 +19,15 @@ public class awss3object {
         System.out.println("hello gurus5");
         System.out.println("hello gurus5 FeatureBranch 1");
 
+    }public  void main2(String args[]){
+
+        System.out.println("hello guru");
+        System.out.println("hello gurus2");
+        System.out.println("hello gurus3");
+        System.out.println("hello gurus4");
+        System.out.println("hello gurus5");
+        System.out.println("hello gurus5 FeatureBranch 1");
+
     }
 
     public void feature1Stashmethod2(){
