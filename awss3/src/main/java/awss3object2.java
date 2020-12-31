8fbc1d5a4b148c55e32c2940bad839e6d78a8b89
Thippu <tippu_gs@yahoo.com>
2020-12-31 06:06:46 -0500

    masterStashmethod1
    Signed-off-by: Thippu <tippu_gs@yahoo.com>

--- awss3/src/main/java/awss3object2.java
+++ awss3/src/main/java/awss3object2.java
@@ -8,4 +8,7 @@ public class awss3object {
         System.out.println("hello gurus4");
         System.out.println("hello gurus5");
     }
+    public void masterStashmethod1(){
+        // wiil visit back
+    }
 }
