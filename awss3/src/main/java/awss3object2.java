a3b31cede9daf1a8e9eceef93673d89601eed406
Thippu <tippu_gs@yahoo.com>
2020-12-31 06:57:10 -0500

    kb checkin
    Signed-off-by: Thippu <tippu_gs@yahoo.com>

--- awss3/src/main/java/awss3object2.java
+++ awss3/src/main/java/awss3object2.java
@@ -1,35 +1,27 @@
 public class awss3object {
-
     public static void main(String args[]){
-
         System.out.println("hello guru");
         System.out.println("hello gurus2");
         System.out.println("hello gurus3");
         System.out.println("hello gurus4");
         System.out.println("hello gurus5");
         System.out.println("hello gurus5 FeatureBranch 1");
-
     }
     public  void main1(String args[]){
-
         System.out.println("hello guru");
         System.out.println("hello gurus2");
         System.out.println("hello gurus3");
         System.out.println("hello gurus4");
         System.out.println("hello gurus5");
         System.out.println("hello gurus5 FeatureBranch 1");
-
     }public  void main2(String args[]){
-
         System.out.println("hello guru");
         System.out.println("hello gurus2");
         System.out.println("hello gurus3");
         System.out.println("hello gurus4");
         System.out.println("hello gurus5");
         System.out.println("hello gurus5 FeatureBranch 1");
-
     }
-
     public void feature1Stashmethod2(){
         // featureStashMethod2
     }
